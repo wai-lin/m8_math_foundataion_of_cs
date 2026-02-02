f18c5c629efc8a6bd0bd3a12cbb3d2aaa4ccd9cf
wai-lin <dev.wailin@gmail.com>
2026-02-02 22:23:39 +0700

    Add functions for approximate area calculation and bisection method

---
+++ 02-bisection_method.py
@@ -0,0 +1,36 @@
+def get_bisection(func, point):
+    a, b = point
+
+    max_iter = 100
+    for i in range(max_iter):
+        c = (a + b) / 2.0
+
+        if f(a)*f(c) < 0:
+            b = c
+        elif f(c)*f(b) < 0:
+            a = c
+        else:
+            if f(a) == 0:
+                c = a
+            elif f(b) == 0:
+                c = b
+
+        print(i, c, f(c))
+
+
+def f(x):
+    return x**2 - 4
+print("==================================================")
+print("f(x) = x**2 - 4")
+print("==================================================\n")
+point = (0, 3)
+get_bisection(f, point)
+
+def f(x):
+    return x**3 - 2 + x**2
+print("\n\n==================================================")
+print("f(x) = x**3 - 2 + x**2")
+print("==================================================\n")
+point = (-1, 5)
+get_bisection(f, point)
+
