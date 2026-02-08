0eff5d41d68e31675e3ce5e3024ac8bc7ff56e1a
wai-lin <dev.wailin@gmail.com>
2026-02-08 22:13:08 +0700

    Implement Gauss-Seidel iteration method for solving linear equations with convergence check

---
+++ src/09-seidel_iter.py
@@ -0,0 +1,91 @@
+import math
+import random
+import numpy as np
+
+
+def euclidean_dist(x1, x2):
+    return np.linalg.norm(np.array(x1) - np.array(x2))
+
+def inf_norm(x1, x2):
+    return np.linalg.norm(np.array(x1) - np.array(x2), ord=np.inf)
+
+def l1_norm(x1, x2):
+    return np.linalg.norm(np.array(x1) - np.array(x2), ord=1)
+
+
+def seidel_iter(A, b, x0=None, eps=1e-10, max_iter=100, debug=False):
+    len_A = len(A)
+    if x0 is None:
+        x0 = [0.0] * len_A
+    x = x0.copy()
+
+    # x1(k+1) = (b1 - a12 * x2(k)) / a11
+    # x2(k+1) = (b2 - a21 * x1(k+1)) / a22
+    # ...
+    for k in range(max_iter):
+        x_new = [0.0] * len_A
+
+        for i in range(len_A):
+            sum_ax = 0.0
+            for j in range(len_A):
+                if j != i:
+                    if j < i:
+                        sum_ax += A[i][j] * x_new[j]
+                    else:
+                        sum_ax += A[i][j] * x[j]
+            x_new[i] = (b[i] - sum_ax) / A[i][i]
+
+        vec_dist = euclidean_dist(x_new, x)
+
+        if debug:
+            print_x = [f'{xi:.6f}' for xi in x_new]
+            print(f"Iteration {k+1}: x = {print_x}")
+
+        if vec_dist < eps:
+            print("Converged!")
+            return x_new
+
+        x = x_new.copy()
+    return x
+
+
+def solve_seidel(A, b, debug=False):
+    print("==============================")
+    print("Gauss-Seidel Iteration Method")
+    print(f"Matrix A: {A}")
+    print(f"Vector b: {b}\n")
+    x_sol = seidel_iter(A, b, debug=debug)
+    print_x = [f"{x:.2f}" for x in x_sol]
+    print(f"\nSolution: {print_x}")
+    print("==============================\n")
+
+
+def gen_matrix(n):
+    J = [[0 for _ in range(n)] for _ in range(n)]
+    b = [random.choice([0, 1]) for _ in range(n)]
+    for i in range(n):
+        for j in range(n):
+            if i == j:
+                J[i][j] = 2
+            elif abs(i - j) == 1:
+                J[i][j] = -1
+            else:
+                J[i][j] = 0
+    return J, b
+
+
+solve_seidel(
+    debug=True,
+    A=[
+        [2, 1],
+        [1, -3]
+    ],
+    b=[3, -2],
+)
+
+A, b = gen_matrix(5)
+solve_seidel(
+    debug=True,
+    A=A,
+    b=b,
+)
